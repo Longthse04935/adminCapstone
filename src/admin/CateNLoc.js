43cfff40bba6f371c56f2496048e591ace09d578
TranDoAnhDuong <42163199+TranDoAnhDuong@users.noreply.github.com>
2019-12-24 13:20:42 +0700

    update validate CateNLoc.js

--- src/admin/CateNLoc.js
+++ src/admin/CateNLoc.js
@@ -9,10 +9,16 @@ class CateNLoc extends Component {
             ],
             categoryData:[
             ],
-            categoryInput:'',
-            locationInput:'',
             data:{
-            }
+              category:'',
+              country:'',
+              city:'',
+              place:''
+            },
+            type:'',
+            imageName:'',
+            isError: false,
+            errors: {},
         }
       }
 
@@ -52,30 +58,60 @@ class CateNLoc extends Component {
     }
 
     onImageChange = async (event) => {
-      let {data} = this.state;
+      let {data,errors} = this.state;
+      
       if (event.target.files && event.target.files[0]) {
         let reader = new FileReader();
+        let imageName = event.target.files[0].name;
+        errors['category'] = '';
       reader.onload = (event) => {
         data['image'] = event.target.result;
-        this.setState({data});
       };
+      this.setState({data,imageName,errors});
       reader.readAsDataURL(event.target.files[0]);
     }
     }
 
+    isValidate = () =>{
+      const { data,type,imageName } = this.state;
+      let isError = false;
+      let errors = {};
+      if(type === 'category'){
+        if(imageName === '' || data.category === '') {
+          isError = true;
+          errors['category'] = 'Category and picture is not empty!';
+        }
+      }else{
+        if(data.country === '' || data.city === '' || data.place === ''){
+          isError = true;
+          errors['location'] = 'Country ,city and place is not empty!!';
+        }
+      }
+      this.setState({isError, errors});
+
+      if (isError) 
+       return true;
+
+      return false;
+    }
+
     handleChange = (e) =>{
-      const { data } = this.state;
+      const { data,errors } = this.state;
         const value = e.target.value;
         const name = e.target.name;
-        
+        errors['category'] = '';
+        errors['location'] = '';
         data[name] = value;
-        this.setState({ data });
+        this.setState({ data,errors});
     }
 
 
-    handleAdd = async (param) => {
+    handleAdd = async (e,param) => {
+      e.preventDefault();
+      if(this.isValidate()) {
+        return false;
+      } 
       let {data} = this.state;
-      console.log(data);
       let autheticate = {
         method: "POST",
         mode: "cors",
@@ -95,7 +131,7 @@ class CateNLoc extends Component {
         if (!response.ok) {
           throw Error(response.status + ": " + response.statusText);
         }
-        this.setState({categoryInput : ''});
+        this.setState({type : '',imageName:''});
       }else if(param === 'location'){
         const response = await fetch(
           Config.api_url + "location/create?country="+data.country+"&city="+data.city+"&place="+data.place,
@@ -105,59 +141,37 @@ class CateNLoc extends Component {
         if (!response.ok) {
           throw Error(response.status + ": " + response.statusText);
         }
-        this.setState({locationInput : ''});
+        this.setState({type : ''});
       }
       this.loadData();
     }
     
-    handleHideAdd = (param) =>{
-      let {categoryInput,locationInput} = this.state;
+    handleHideAdd = (param ) =>{
+     let type,imageName;
+     let {data,errors} = this.state;
       if(param === 'category'){
-        categoryInput = '';
+        type = '';
+        imageName='';
+        data['image'] = '';
+        errors['category'] = ''
       }else{
-        locationInput = '';
+        type = '';
+        errors['location'] = ''
       }
-      this.setState({categoryInput,locationInput});
+      this.setState({type,imageName,data,errors});
    
     }
 
   showInput = (param) => {
-    let {categoryInput,locationInput} = this.state;
-    if(param === 'category'){
-      categoryInput = <div className="form-group categoryForm">
-                        {/* <input type="text" className="form-control addForm_Input" id="exampleFormControlInput1" name="category" onChange={this.handleChange} placeholder="Category"/> */}
-                        <div style={{marginBottom:'20px'}}>
-                        <input type="text" className="form-control addForm_Input" placeholder="Category" name="category" onChange={this.handleChange} placeholder="Category"/>
-                        <label htmlFor="file-upload" className="custom-file-upload"> Upload image category
-                        </label>
-                        <input id="file-upload" type="file" name="image" onChange={this.onImageChange}/>
-                        </div>
-                        <span className="btn btn-primary btn-icon-split triggerA" onClick={()=>this.handleAdd('category')}>
-                          <span className="text">Add</span>
-                        </span>
-                        <span style={{marginLeft:'5px'}} className="btn btn-primary btn-icon-split triggerA" onClick={()=>this.handleHideAdd('category')}>
-                          <span className="text">Close</span>
-                        </span>
-                      </div>
-    }else{
-      locationInput = <div className="form-group locationForm">
-                        <div style={{marginBottom:'20px'}}>
-                        <input type="text" className="form-control locationForm_input" name="country" onChange={this.handleChange} placeholder="Country"/>
-                        <input type="text" className="form-control locationForm_input"  name="city" onChange={this.handleChange} placeholder="City"/>
-                        <input type="text" className="form-control locationForm_input" name="place" onChange={this.handleChange} placeholder="Place"/>
-                        </div>
-                        <span className="btn btn-primary btn-icon-split triggerA " onClick={()=>this.handleAdd('location')}>
-                          <span className="text">Add</span>
-                        </span>
-                        <span style={{marginLeft:'5px'}} className="btn btn-primary btn-icon-split triggerA" onClick={()=>this.handleHideAdd('location')}>
-                          <span className="text">Close</span>
-                        </span>
-                      </div>
-    }
-    this.setState({categoryInput,locationInput})
+    let {data,imageName,errors} = this.state;
+    data['image'] = '';
+    imageName = '';
+    errors['location'] = '';
+    errors['category'] = '';
+    this.setState({type:param,data,imageName,errors});
   }
   render() {
-    let {categoryInput,locationInput,categoryData,locationData,image} = this.state;
+    let {type,categoryData,locationData,imageName,errors} = this.state;
     let category = categoryData.map((data, index) => (
         <tr role="row" className="odd" key={index}>
         <td className="sorting_1">{data.category} trip</td>
@@ -195,7 +209,27 @@ class CateNLoc extends Component {
                         <span className="btn btn-primary btn-icon-split guider-button" onClick={()=>this.showInput('category')}>
                           <span className="text">Add category</span>
                         </span>
-                        {categoryInput}
+                        {
+                          type === 'category' ?
+                          <div className="form-group categoryForm">
+                            {/* <input type="text" className="form-control addForm_Input" id="exampleFormControlInput1" name="category" onChange={this.handleChange} placeholder="Category"/> */}
+                            <div style={{marginBottom:'20px'}}>
+                            <input type="text" className="form-control addForm_Input" placeholder="Category" name="category" onChange={this.handleChange} placeholder="Category"/>
+                            <label htmlFor="file-upload" className="custom-file-upload"> Upload image category
+                            </label>
+                            <input id="file-upload" type="file" name="image" onChange={this.onImageChange}/>
+                            </div>
+                            <p style={{color:'black'}}>{imageName}</p>
+                            {errors['category'] ? <p style={{color: "red"}} className="errorInput">{errors['category']}</p> : ''}
+                            <span className="btn btn-primary btn-icon-split triggerA" onClick={(e)=>this.handleAdd(e,'category')}>
+                              <span className="text">Add</span>
+                            </span>
+                            <span style={{marginLeft:'5px'}} className="btn btn-primary btn-icon-split triggerA" onClick={()=>this.handleHideAdd('category')}>
+                              <span className="text">Close</span>
+                            </span>
+                          </div>
+                          : null
+                        }
                       </div>
                     </div>
                     <div className="row">
@@ -249,7 +283,24 @@ class CateNLoc extends Component {
                         <span className="btn btn-primary btn-icon-split guider-button" onClick={()=>this.showInput('location')}>
                           <span className="text">Add location</span>
                         </span>
-                        {locationInput}
+                        {
+                          type === 'location' ?
+                          <div className="form-group locationForm">
+                        <div style={{marginBottom:'20px'}}>
+                        <input type="text" className="form-control locationForm_input" name="country" onChange={this.handleChange} placeholder="Country"/>
+                        <input type="text" className="form-control locationForm_input"  name="city" onChange={this.handleChange} placeholder="City"/>
+                        <input type="text" className="form-control locationForm_input" name="place" onChange={this.handleChange} placeholder="Place"/>
+                        </div>
+                        {errors['location'] ? <p style={{color: "red"}} className="errorInput">{errors['location']}</p> : ''}
+                        <span className="btn btn-primary btn-icon-split triggerA " onClick={(e)=>this.handleAdd(e,'location')}>
+                          <span className="text">Add</span>
+                        </span>
+                        <span style={{marginLeft:'5px'}} className="btn btn-primary btn-icon-split triggerA" onClick={()=>this.handleHideAdd('location')}>
+                          <span className="text">Close</span>
+                        </span>
+                      </div>
+                      : null
+                        }
                       </div>
                     </div>
                     <div className="row">
